38d65f42a2695db7cc809887934cb5bb21f2617a
tateuniversity4 <142125473+tateuniversity4@users.noreply.github.com>
2023-08-13 00:44:24 +0100

    Add files via upload

---
+++ js/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["/[...slug]","/e/[...slug]","/ps/[id]","/resources/[...slug]","/vig/[channelId]","/vp/[id]","/vpp/[id]"]),self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB();
